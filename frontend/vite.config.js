c3b254eb5a416bc628e194cd84a1e4e19cbba001
maralianannya <maralianannya@gmail.com>
2026-03-31 12:06:47 +0530

    updated full frontend folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // This tells Vite that the Framer link is an external dependency
-      external: [/^https:\/\/framer\.com\/m\/.*/],
-    },
-  },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
